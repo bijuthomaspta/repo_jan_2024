ce984a223b4641c6cfbb657b71cde37412674701
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2024-01-08 15:35:23 +0530

    Update main.tf

--- projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -27,7 +27,7 @@ data "aws_subnets" "subnets" {
 module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster5856666"
-  cluster_version = "1.23"
+  #cluster_version = "1.23"
   #subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
   subnet_ids = data.aws_subnets.subnets.ids
   vpc_id          = aws_default_vpc.default.id
@@ -46,43 +46,36 @@ module "in28minutes-cluster" {
 }
 
 
-# data "aws_eks_cluster" "cluster" {
-#   name = module.in28minutes-cluster.cluster_name
-#   depends_on = [module.in28minutes-cluster.cluster_name]
-# }
+data "aws_eks_cluster" "cluster" {
+  name = module.in28minutes-cluster.cluster_name
+  depends_on = [module.in28minutes-cluster.cluster_name]
+}
 
 
-# data "aws_eks_cluster_auth" "cluster1" {
-#   name = module.in28minutes-cluster.cluster_name
-#   depends_on = [module.in28minutes-cluster.cluster_name]
-# }
+data "aws_eks_cluster_auth" "cluster1" {
+  name = module.in28minutes-cluster.cluster_name
+  depends_on = [module.in28minutes-cluster.cluster_name]
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#   token = data.aws_eks_cluster_auth.cluster1.token
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster1.token
+}
 
-
+resource "kubernetes_service_account" "example" {
+  metadata {
+    name      = "default"
+    namespace = "dfault"
+  }
+  
+  automount_service_account_token = true
+}
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
 # ServiceAccount needs permissions to create deployments 
 # and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
-}
+
 
 # Needed to set the default region
 provider "aws" {
