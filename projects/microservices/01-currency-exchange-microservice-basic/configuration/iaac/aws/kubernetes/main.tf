297222b82902aa318ffacb5b6aba9c34be5d36ed
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2024-01-08 21:55:23 +0530

    Update main.tf

--- projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -64,19 +64,15 @@ provider "kubernetes" {
 }
 module "iam_eks_role" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "my-app"
+  role_name = "in28minutes-cluster5856666-cluster-20240108144616335400000001"
 
   role_policy_arns = {
-    policy = "arn:aws:iam::012345678901:policy/myapp"
+    policy = "arn:aws:iam::784395903625:role/in28minutes-cluster5856666-cluster-20240108144616335400000001"
   }
 
   oidc_providers = {
     one = {
-      provider_arn               = "arn:aws:iam::012345678901:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/5C54DDF35ER19312844C7333374CC09D"
-      namespace_service_accounts = ["default:my-app-staging"]
-    }
-    two = {
-      provider_arn               = "arn:aws:iam::012345678901:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/5C54DDF35ER54476848E7333374FF09G"
+      provider_arn               = "arn:aws:iam::784395903625:oidc-provider/oidc.eks.ap-south-1.amazonaws.com/id/ED17138BD44C3751F3C24CF9C71DAB42"
       namespace_service_accounts = ["default:my-app-staging"]
     }
   }
