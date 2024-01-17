4f8ea28138549d87e2efcead4fa6e8b5da401980
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2024-01-17 22:18:11 +0530

    Update main.tf

--- projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
+++ projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/aws/kubernetes/main.tf
@@ -89,7 +89,7 @@ resource "kubernetes_cluster_role_binding" "example" {
     name = "cluster-rolebinding-for-default"
   }
   role_ref {
-    api_group = ["*"]
+    api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
     name      = "cluster-role-for-default"
   }
